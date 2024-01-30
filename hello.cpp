3f68aac5a725c9a78b8c2ed56cf85a6f39e170a6
brandon <brandonchanchez25@gmail.com>
2024-01-30 14:48:33 -0800

    forgot comments

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+
+
+
+/*** 
+ 
+ * This lab just prints hellow world to the console 
+  ***/
+
+
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!\n" << endl;
+	printf("Hello world in C!!\n");
+
+	return 0;
+}
+
+
